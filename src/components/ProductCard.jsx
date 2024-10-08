92bc36f604efc0a125acc030c105b81a68e47e0f
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-10-08 20:25:52 +0530

    lazy load

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-
+import LazyLoad from 'react-lazyload';
 //Products should show in card wise using productcard fn with destructured from props (Home component)
 const ProductCard = ({ product, onClick }) => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer" onClick={onClick}>
-      <img
-        src={product.image}
-        alt={product.name}
-        className="w-full h-48 object-cover"
-      />
+      <LazyLoad height={200} offset={100}>
+  <img src={product.image} alt={product.name} />
+</LazyLoad>
       <div className="p-4">
         <h2 className="text-lg font-semibold text-gray-800">{product.name}</h2>
         <p className="text-gray-600">{product.description}</p>
